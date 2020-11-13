fd1b038c9a07018bbdfcec0d71f6efb83b54e493
rankem <ankem@live.com>
2020-11-12 19:19:40 -0600

    add simple unit tests to run in build pipeline

---
+++ test/test_CustomNumbersTest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from src.CustomNumbers import addNumbers
+
+
+class TestSetup(unittest.TestCase):
+
+    def setUp(self):
+        self.a = 10
+        self.b = 20
+
+    def testAddNumbers(self):
+        c = addNumbers(self.a, self.b)
+        self.assertEqual(30, c)
+
+
+if __name__ == '__main__':
+    unittest.main()
